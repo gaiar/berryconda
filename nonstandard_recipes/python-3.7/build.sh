b9975a9b458f624e2075f8249576184f62e29f29
gaiar <gaiar@baimuratov.ru>
2018-08-05 14:11:09 +0200

    Add Python 3.7

---
+++ nonstandard_recipes/python-3.7/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python ${RECIPE_DIR}/brand_python.py
+
+# Remove test data and ensurepip stubs to save space
+rm -rf Lib/test Lib/*/test
+rm -rf Lib/ensurepip
+
+./configure --enable-shared --enable-ipv6 --with-ensurepip=no \
+	--enable-optimizations \
+	--prefix=$PREFIX \
+	--with-tcltk-includes="-I$PREFIX/include" \
+	--with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
+	CPPFLAGS="-I$PREFIX/include" \
+	LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
+
+make -j${CPU_COUNT}
+make install
+ln -s $PREFIX/bin/python3.7 $PREFIX/bin/python
+ln -s $PREFIX/bin/pydoc3.7 $PREFIX/bin/pydoc
