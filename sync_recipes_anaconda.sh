ea858ea2f35cd1a2c263077069f66f136e77da31
gaiar <gaiar@baimuratov.ru>
2018-08-30 13:16:50 +0200

    [WIP] Sync recipes from Anacoda feedstocks

---
+++ sync_recipes_anaconda.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+URL="https://github.com/AnacondaRecipes/aggregate/"
+
+"svn checkout https://github.com/lodash/lodash/trunk/docs"
+
+svn checkout https://github.com/AnacondaRecipes/aggregate/trunk/ctng-compilers-activation-feedstock
