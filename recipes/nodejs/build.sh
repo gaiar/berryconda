c7422bf334a8a62ddc8fe3c4785c4a3ee7b0c60c
gaiar <gaiar@baimuratov.ru>
2018-12-27 13:04:31 +0100

    update nodejs

--- recipes/nodejs/build.sh
+++ recipes/nodejs/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# scrub -std=... flag which conflicts with builds
+export CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-std=[^ ]*@@g')
+
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --without-snapshot
-make #-j${CPU_COUNT}
+make -j${CPU_COUNT}
 make install
 
 node -v
