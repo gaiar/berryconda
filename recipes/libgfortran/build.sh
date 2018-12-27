7a621801c276982e2b8b721b9c2f76fb9d9718b4
gaiar <gaiar@baimuratov.ru>
2018-12-27 12:45:53 +0100

    add aarch64 support for libgfortran

--- recipes/libgfortran/build.sh
+++ recipes/libgfortran/build.sh
@@ -12,3 +12,7 @@ if [ `uname -m` == 'armv6l' ]; then
     cp /usr/lib/arm-linux-gnueabihf/libgfortran.so.3.0.0 .
     ln -s libgfortran.so.3.0.0 libgfortran.so.3
 fi
+if [ `uname -m` == 'aarch64' ]; then
+    cp /usr/lib/aarch64-linux-gnu/libgfortran.so.3.0.0 .
+    ln -s libgfortran.so.3.0.0 libgfortran.so.3
+fi
